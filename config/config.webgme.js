8cd473ce45d969670630326dd25cca0a4f086c45
Patrik Meijer <patrik.meijer@vanderbilt.edu>
2019-01-23 15:37:21 -0600

    webgme new viz

--- config/config.webgme.js
+++ config/config.webgme.js
@@ -10,14 +10,17 @@ var config = require('webgme/config/config.default'),
 
 
 
+config.visualization.panelPaths.push(__dirname + '/../src/visualizers/panels');
 
 
 
 
 // Visualizer descriptors
-
+config.visualization.visualizerDescriptors.push(__dirname + '/../src/visualizers/Visualizers.json');
 // Add requirejs paths
 config.requirejsPaths = {
+  'panels': './src/visualizers/panels',
+  'widgets': './src/visualizers/widgets',
   'hierarchical-model-editor': './src/common'
 };
 
